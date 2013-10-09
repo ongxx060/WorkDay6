884322d6a500264c22ccedc0d1efcfe7f7adc33e
idsc3101 <idsc3101@gmail.com>
2013-10-09 15:14:45 -0500

    commit

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -3,7 +3,17 @@
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
 
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+
+        Dim a As Integer
+
+        Do While (a <= 10)
+            lstResult.Items.Add("number is" & a ^ 2)
+            a = a + 1
+        Loop
+
+    End Sub
+
+    Private Sub lstResult_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles lstResult.SelectedIndexChanged
 
     End Sub
 End Class
